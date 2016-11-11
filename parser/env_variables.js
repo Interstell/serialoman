2126bafb9796d36fdb5ffd0108da4de33462bdbf
Interstell <urukovdima@gmail.com>
2016-11-11 21:54:27 +0200

    Revert "CW: express project initialized"
    This reverts commit 66f3bed93a1324ca90dd8e57b1752a9c132a053d.

--- parser/env_variables.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  lostfilm:{
-    email:'serialoman.kpi@yandex.ru',
-    login:'serialoman.kpi',
-    pass:'serialoman',
-    cookies:'lnk_uid=28004dff151d29ca115dc8637a67fd24; pass=26a1d62b1ae0fef6635f9004de414fd9; uid=5129322'
-  }
-};
