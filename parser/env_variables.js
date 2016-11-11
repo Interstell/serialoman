66f3bed93a1324ca90dd8e57b1752a9c132a053d
Interstell <urukovdima@gmail.com>
2016-11-11 21:52:24 +0200

    CW: express project initialized

---
+++ parser/env_variables.js
@@ -0,0 +1,8 @@
+module.exports = {
+  lostfilm:{
+    email:'serialoman.kpi@yandex.ru',
+    login:'serialoman.kpi',
+    pass:'serialoman',
+    cookies:'lnk_uid=28004dff151d29ca115dc8637a67fd24; pass=26a1d62b1ae0fef6635f9004de414fd9; uid=5129322'
+  }
+};
