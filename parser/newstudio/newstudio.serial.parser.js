d490ea74bdd1b28a0a1ec9da62286ff86e160614
Interstell <urukovdima@gmail.com>
2016-12-08 21:55:14 +0200

    newstudio serial parser in progress

--- parser/newstudio/newstudio.serial.parser.js
+++ parser/newstudio/newstudio.serial.parser.js
@@ -84,12 +84,40 @@ exports.getSerialsOriginalNames = function (serials) {
             }, 250);
         })
     }
+    function getInfoFromEpisodePage(serial){
+        return new Promise((resolve, reject)=>{
+            request({uri:serial.topic_url, headers : { Cookie : process.env.NEWSTUDIO_COOKIES },
+                method:'GET'}, (err, res, page) => {
+                if (!err && res.statusCode == 200) {
+                    let $ = cheerio.load(page, {decodeEntities: false});
+                    let body = $('.post_body').text();
+                    let content = body.match(/Год выхода: ([\d]+)/);
+                    serial.start_year = content?parseInt(content[1]):null;
+                    content = body.match(/Жанр: ([А-Яа-я+ ,]+)Режиссер/);
+                    serial.genres = content?content[1].match(/([^,]+)/g).map(el => el.trim().toLowerCase()):null;
+                    content = body.match(/Режиссер: ([^:]+)В ролях/);
+                    serial.directors = content?content[1]:null;
+                    content = body.match(/В ролях: ([^:]+)О фильме/);
+                    serial.actors = content?content[1]:null;
+                    content = body.match(/О фильме:([^|]+)Над релизом/);
+                    serial.description = content?content[1]:null;
+                    resolve(serial);
+                }
+            });
+        })
+    }
+    let current_serial;
     let sequence = Promise.resolve();
     for (let i = 0; i < serials.length; i++){
         sequence = sequence
             .then(() => getSerialName(serials[i]))
-            .then(serial => console.log(serial))
+            .then(serial => {
+                current_serial = serial;
+            })
             .then(() => waitSomeTimeBecauseOfDos())
+            .then(() => getInfoFromEpisodePage(current_serial))
+            .then(serial => console.log(serial))
+            .then(() => waitSomeTimeBecauseOfDos());
     }
     return sequence.then(() => Promise.resolve(serials));
 };
@@ -119,13 +147,37 @@ exports.filterOMDBData = function(serials){
         && serial.omdb_data.Year.length == 5 //e.g. 2012-
         && serial.omdb_data.Type == 'series'
     );
-}
+};
 
-/*exports.parseSerialNamesFromIndexPage()
+exports.fillObjectToSuitModel = function (serial) {
+    return{
+        name: serial.rus_name + ' ('+serial.orig_name+')',
+        rus_name:serial.rus_name,
+        orig_name: serial.orig_name,
+        url: serial.url,
+        source: serial.source,
+        poster: serial.omdb_data.Poster, //todo change to newstudio's
+        prod_country: serial.omdb_data.Country,
+        //start_year: parseInt(serial.omdb_data.Year.match(/([\d]+)/)[1]),
+        //genres: serial.omdb_data.Genre.match(/([^,]+)/g).map(el => el.trim()),
+        seasons_num: parseInt(serial.omdb_data.totalSeasons),
+        is_on_air: true,
+        own_site: null,
+        //description: null,
+        //actors: (serial.omdb_data.Actors && serial.omdb_data.Actors != 'N/A')?serial.omdb_data.Actors:null,
+        //directors: (serial.omdb_data.Director && serial.omdb_data.Director != 'N/A')?serial.omdb_data.Director:null,
+        scriptwriters: (serial.omdb_data.Writer && serial.omdb_data.Writer != 'N/A')?serial.omdb_data.Writer:null,
+        plot: (serial.omdb_data.Plot && serial.omdb_data.Plot != 'N/A')?serial.omdb_data.Plot:null
+    }
+};
+
+
+exports.parseSerialNamesFromIndexPage()
     .then(serials => exports.getSerialsOriginalNames(serials))
     .then(serials => serials.filter(serial => serial.orig_name))
-    .then(serials => console.log(serials));*/
-
-exports.getOMDBData(sampleSerials)
-    .then(serials => exports.filterOMDBData(serials))
     .then(serials => console.log(serials));
+
+/*exports.getOMDBData(sampleSerials)
+    .then(serials => exports.filterOMDBData(serials))
+    .then(serials => serials.map(serial => exports.fillObjectToSuitModel(serial)))
+    .then(serials => console.log(serials));*/
